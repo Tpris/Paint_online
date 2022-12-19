366a5ce8b49fc7df719ebe9fb757396009e90bbb
Tpris <75440348+Tpris@users.noreply.github.com>
2022-12-19 17:46:21 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+const canvas = document.getElementById('drawing-board');
+const ctx = canvas.getContext('2d');
+const lineWidth = document.getElementById('lineWidth');
+const color = document.getElementById('color');
+let mouseDown = false;
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+ctx.lineWidth = lineWidth.value;
+ctx.strokeStyle = color.value;
+ctx.lineCap = 'round';
+
+document.getElementById('erase').onclick = function(){
+    ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
+}
+
+color.onclick = function(e){
+    ctx.strokeStyle = e.target.value;
+}
+
+lineWidth.onchange = function(e){
+    ctx.lineWidth = e.target.value;
+}
+
+canvas.onmousedown = function() { 
+    mouseDown = true;
+    ctx.beginPath();
+}
+canvas.onmouseup = function() {
+    mouseDown = false;  
+}
+
+canvas.onmousemove = function(e) {
+    if(mouseDown){
+        ctx.stroke();
+        ctx.lineTo(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop);
+    }
+}
