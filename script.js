20c703aeb921f0ce6db78cd343f220db7cb3c4a3
Tpris <priscill@live.fr>
2023-01-12 22:06:45 +0100

    :rewind: Remise en état

--- script.js
+++ script.js
@@ -4,14 +4,21 @@ const lineWidth = document.getElementById('lineWidth');
 const color = document.getElementById('color');
 const saveButton = document.getElementById('save');
 const eraserBrush = document.getElementById('eraser-brush');
+const img = document.getElementById('img');
+
 let mouseDown = false;
 let erase = false;
 
-canvas.width = window.innerWidth - canvas.offsetLeft;
-canvas.height = window.innerHeight;
-ctx.lineWidth = lineWidth.value;
-ctx.strokeStyle = color.value;
-ctx.lineCap = 'round';
+
+initialiseCanvas(window.innerWidth - canvas.offsetLeft, window.innerHeight)
+
+function initialiseCanvas(width,height){
+    canvas.width = width;
+    canvas.height = height;
+    ctx.lineWidth = lineWidth.value;
+    ctx.strokeStyle = color.value;
+    ctx.lineCap = 'round';
+}
 
 document.getElementById('erase').onclick = function(){
     ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
@@ -50,7 +57,7 @@ canvas.onmouseup = canvas.ontouchend = function() {
 canvas.onmousemove = function(e) {
     if(mouseDown){
         ctx.stroke();
-        ctx.lineTo(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop);
+        ctx.lineTo(e.clientX - canvas.offsetLeft, e.clientY);
     }
 }
 
@@ -61,6 +68,11 @@ canvas.ontouchmove = function(e) {
     }
 }
 
+canvas.onclick = function(e){
+    ctx.fillStyle=color.value;
+    ctx.fillRect(e.clientX - canvas.offsetLeft,e.clientY - canvas.offsetTop,1,1)
+}
+
 saveButton.onclick = function() {
   let imageName = prompt('Image name :', 'paint');
   let a = document.createElement('a');
@@ -69,85 +81,34 @@ saveButton.onclick = function() {
   a.click();
 }
 
-function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
-  }
-  
-// add stars
-const starContainer = document.createElement('div');
-starContainer.className="glitter-star";
-document.body.appendChild(starContainer);
-for (let i = 0; i < 50; i++) {
-    const star = document.createElement('span')
-    star.innerHTML = "ac_unit";
-    star.className = "material-symbols-outlined";
-    star.style.top = Math.random() * 100 + "%";
-    star.style.left = Math.random() * 100 + "%";
-    star.style.fontSize = Math.random() * 50 + "px";
-    star.style.animationDelay = Math.random() *1.5 + 's';
-    star.style.animationDuration = Math.random() * 10;
-    star.style.color = "rgb("+getRandomInt(0,255)+","+getRandomInt(0,255)+","+getRandomInt(0,255)+")";
-    starContainer.appendChild(star);
+document.getElementById('upload').onchange = function(){
+    let file = this.files[0];
+    let reader = new FileReader();
+
+    reader.readAsDataURL(file);
+
+    reader.onload = function () {
+        img.src = reader.result;
+    }
 }
 
-
-function addIcon(className){
-    const star = document.createElement('span')
-    star.className = className;
-    star.style.top = Math.random() * 100 + "%";
-    star.style.left = Math.random() * 100 + "%";
-    star.style.fontSize = Math.random() * 50 + "px";
-    star.style.animationDelay = Math.random() *1.5 + 's';
-    star.style.animationDuration = Math.random() * 10;
-    star.style.color = "rgb("+getRandomInt(0,255)+","+getRandomInt(0,255)+","+getRandomInt(0,255)+")";
-    starContainer.appendChild(star);
+img.onload = function(){
+    initialiseCanvas(img.width,img.height);
 }
 
-for (let i = 0; i < 50; i++) {
-    addIcon("icofont-star-alt-1 icofont-2x");
-    addIcon("icofont-panda-face icofont-2x");
-    addIcon("icofont-panda icofont-3x");
-    addIcon("icofont-horse-head icofont-3x");
-    addIcon("icofont-octopus icofont-3x");
-    addIcon("icofont-octopus-alt icofont-3x");
+document.getElementById('upload-canvas').onchange = function(){
+    let file = this.files[0];
+    let reader = new FileReader();
+
+    if(file)
+        reader.readAsDataURL(file);
+    
+    reader.onload = function () {
+        let imgScribble = new Image();
+        imgScribble.src = reader.result;
+        imgScribble.onload = function () {
+            ctx.drawImage(imgScribble, 0, 0);
+        }
+    }
 }
-
-//pere noel
-
-const pereNoel = document.getElementById("pere-noel");
-pereNoel.style.left = (innerWidth/2 -150)+"px";
-pereNoel.style.top = (innerHeight/2 -150)+"px";
-
-const laser1 = document.getElementById("laser1");
-laser1.style.left = (innerWidth/2-70)+"px";
-laser1.style.top = (innerHeight/2+20)+"px";
-
-const laser2 = document.getElementById("laser2");
-laser2.style.left = (innerWidth/2+15)+"px";
-laser2.style.top = (innerHeight/2+20)+"px";
-
-const laserMove = [
-    { transform: 'rotate(-23deg)', opacity:1 },
-    { transform: 'rotate(-23deg) translate('+(innerWidth)+"px,"+(innerHeight/-8)+"px)", opacity:1, offset:0.48},
-
-    { transform: 'rotate(-23deg) translate('+(innerWidth)+"px,"+(innerHeight/-8)+"px)", opacity:0, offset:0.49},
-    { transform: 'rotate(-23deg) translate('+(-innerWidth)+"px,"+(innerHeight/8)+"px)", opacity:0, offset:0.5},
-
-    { transform: 'rotate(23deg)', opacity:1, offset:0.51 },
-    { transform: 'rotate(23deg) translate('+(innerWidth)+"px,"+(innerHeight/8)+"px)", opacity:1, offset:0.98},
-
-    { transform: 'rotate(23deg) translate('+(innerWidth)+"px,"+(innerHeight/8)+"px)", opacity:0, offset:0.99},
-    { transform: 'rotate(23deg) translate('+(-innerWidth)+"px,"+(innerHeight/-8)+"px)", opacity:0}
-];
-
-const timing = {
-    duration: 3000,
-    iterations: Infinity
-}
-
-laser1.animate(laserMove,timing);
-laser2.animate(laserMove,timing);
-
 
